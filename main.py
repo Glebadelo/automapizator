72d5b16da51ed0e79498de946e37e5838edceda9
gleb <glebs01@mail.ru>
2022-07-04 16:48:50 +0700

    Реализован конфигурационный файл и добавлены чекбоксы для других модулей

--- main.py
+++ main.py
@@ -15,20 +15,34 @@ from datetime import date
 
 import resources_rc
 
+#Работа с конфигкрационным файлом
+
+try:                                                                   # Модуль для работы с файлами конфигурации
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+
 work_path = os.getcwd()
+
+file_settings = r'Config.ini'
 
 #Список зон для запросов
 zonesKeys = ['Ob_reservoir', 'Baikal_lake']
 
 
 #наборы тайлов
-Ob_reservoir = ('44UPF', '44UNF', '44UNE')
-Baikal_lake = ('49UCB', '49UDB', '49UCA', '49UCV')
+#Ob_reservoir = ('44UPF', '44UNF', '44UNE')
+# Baikal_lake = ('49UCB', '49UDB', '49UCA', '49UCV')
 
-#словарь названий со значением списка тайлов
-tiles = {}
-tiles['Ob_reservoir'] = Ob_reservoir
-tiles['Baikal_lake'] = Baikal_lake
+tiles = [('44UPF', '44UNF', '44UNE'), ('49UCB', '49UDB', '49UCA', '49UCV')]
+print('tiles до = {}'.format(tiles))
+print('tiles до = {}'.format(type(tiles)))
+# словарь названий со значением списка тайлов
+zoneTiles = {}
+for zone in zonesKeys:
+    zoneTiles[zone] = tiles[zonesKeys.index(zone)]
+print(zoneTiles)
+
 
 
 zones = ('Обское водохранилище', 'Озеро Байкал') #список зон
@@ -40,7 +54,136 @@ password = 'gordeeva120689'
 product_type = 'S2MSI1C'
 platform = 'Sentinel-2'
 download_path = 'downloads'
+path_archive = 'D:/PycharmProjects/automapizator/folder'
 
+list_settings = []
+
+
+def create_config_SAD(path):
+    """Функция создания файла конфигурации,
+       с опциями "по умолчанию", которые "зашиты"
+       в программе, если файл Config.ini
+       будет отсутствовать
+    """
+    try:
+        config = configparser.ConfigParser()  # Создаём файл конфигурации
+        config.add_section('Settings_ESA')
+        config.add_section('Zones_names')
+        config.add_section('Zones_tiles')
+        config.add_section('Settings_paths')  # Создаём секцию [Settings_paths]
+
+        # Добавляем опций в секцию
+        config.set('Settings_ESA', 'address', 'https://scihub.copernicus.eu/dhus')  # адрес сервиса Sentinel-1-2-3
+        config.set('Settings_ESA', 'login', 'helga1289')  # логин
+        config.set('Settings_ESA', 'password', 'gordeeva120689')  # пароль
+        config.set('Settings_ESA', 'platform', 'Sentinel-2')  # имя платформы
+        config.set('Settings_ESA', 'product_type', 'S2MSI1C')  # тип продукта
+        config.set('Zones_names', 'zones', 'Обское_водохранилище Озеро_Байкал') # список имён зон для отображения
+        config.set('Zones_names', 'zonesKeys', 'Ob_reservoir Baikal_lake')
+        config.set('Zones_tiles', 'Ob_reservoir', '44UPF 44UNF 44UNE')      # список имён тайлов зоны
+        config.set('Zones_tiles', 'Baikal_lake', '49UCB 49UDB 49UCA 49UCV')  # список имён тайлов зоны
+        config.set('Settings_paths', 'path_downloads','downloads')  # каталог для скаченных файлов
+        config.set('Settings_paths', 'path_archive',
+                    'D:/PycharmProjects/automapizator/folder')
+
+        with open(path,
+                  "w") as config_file:  # Открывает файл (или создаёт, если отсутствует) для записи в режиме write only
+            config.write(config_file)
+        print('Файл конфигурации успешно создан: {}'.format(path))
+    except:
+        print('Ошибка создания файла конфигурации. Не удалось создать файл конфигурации')
+
+
+def get_config_SAD(path):
+    '''Функция получения объекта файла конфигурации
+    '''
+
+    try:
+        if not os.path.exists(path):  # если файл конфигурации не найден,
+            create_config_SAD(path)  # то файл конфигурации создатся, используя настройки по умолчанию
+
+        config = configparser.ConfigParser()
+        config.read(path)  # считываем файл конфигурации
+
+        return config
+
+    except:
+        print('Ошибка доступа к файлу конфигурации')
+
+
+def get_settings_config_SAD(path, section, settings):
+    '''Функция получения информации из опций файла конфигурации
+    '''
+    config = get_config_SAD(path)  # получение файла конфигурации
+    value_section = config.get(section, settings)  # получение значения опции из секции
+
+    if section == 'list_tiles' or section == 'Zones_names':
+        return value_section.split(' ')  # преобразуем строку в список, разделяя элементы по пробелам
+    else:
+        return value_section  # возвращает опцию в виде строки
+
+def get_tiles_config_SAD(path, section):
+    tiles = []
+    config = get_config_SAD(path)
+    for zone in config[section]:
+        value_section = config.get(section, zone)
+        tiles.append(value_section.split(' '))
+    return tiles
+
+def create_list_settings(path):
+    ''' Функция заполнения списка данными из файла конфигурации.
+         В случае отсутсвия доступа к каталогу с файлом конфигурации -
+         список будет сформирован из значений по умолчанию.
+    '''
+
+    if os.path.exists(os.path.split(path)[
+                          0]):  # проверка: существует ли данный путь при помощи метода split получаем множество из двух элементов: пути и файла
+        try:
+            list_settings.append(get_settings_config_SAD(path, 'Settings_ESA', 'address', ))
+            list_settings.append(get_settings_config_SAD(path, 'Settings_ESA', 'login', ))
+            list_settings.append(get_settings_config_SAD(path, 'Settings_ESA', 'password', ))
+            list_settings.append(get_settings_config_SAD(path, 'Settings_ESA', 'platform'))
+            list_settings.append(get_settings_config_SAD(path, 'Settings_ESA', 'product_type'))
+            list_settings.append(get_settings_config_SAD(path, 'Zones_names', 'zones'))
+            list_settings.append(get_settings_config_SAD(path, 'Zones_names', 'zonesKeys'))
+            list_settings.append(get_tiles_config_SAD(path, 'Zones_tiles'))
+            list_settings.append(get_settings_config_SAD(path, 'Settings_paths', 'path_downloads'))
+            list_settings.append(get_settings_config_SAD(path, 'Settings_paths', 'path_archive'))
+
+            # address = get_settings_config_SAD(path, 'Settings_ESA', 'address', )
+            # login = get_settings_config_SAD(path, 'Settings_ESA', 'login', )
+            # password = get_settings_config_SAD(path, 'Settings_ESA', 'password', )
+            # platform = get_settings_config_SAD(path, 'Settings_ESA', 'platform')
+            # product_type = get_settings_config_SAD(path, 'Settings_ESA', 'product_type')
+            # tiles = get_tiles_config_SAD(path, 'Zones_tiles')
+            # download_path = get_settings_config_SAD(path, 'Settings_paths', 'path_downloads')
+            # path_archive = get_settings_config_SAD(path, 'Settings_paths', 'path_archive')
+
+            print('Файл конфигурации {} успешно прочтён'.format(path))
+
+        except:
+            print(
+                'Ошибка конфигурации. Не удалось создать файл конфигурации, будут применены параметры по умолчанию')
+
+
+create_list_settings(work_path + os.sep + file_settings)
+address = list_settings[0]
+login = list_settings[1]
+password = list_settings[2]
+platform = list_settings[3]
+product_type = list_settings[4]
+zones = list_settings[5]
+zonesKeys = list_settings[6]
+tiles = list_settings[7]
+download_path = list_settings[8]
+path_archive = list_settings[9]
+print('tiles после = {}'.format(tiles))
+print('tiles после = {}'.format(type(tiles)))
+
+zoneTiles = {}
+for zone in zonesKeys:
+    zoneTiles[zone] = tiles[zonesKeys.index(zone)]
+print(zoneTiles)
 
 
 def resource_path(relative):
@@ -84,19 +227,19 @@ class copyFiles(QThread):
         dateFolder = a.replace('-', '')
         print(dateFolder)
 
-        if os.path.isdir('D:/folder/' + dateFolder):
+        if os.path.isdir('D:/PycharmProjects/' + dateFolder):
             for folder in folders:
                 files = os.listdir(work_path + os.sep + download_path + os.sep + folder)
                 for file in files:
                     shutil.copy(work_path + os.sep + download_path + os.sep + folder + os.sep + file,
-                                'D:/folder/' + dateFolder)
+                                'D:/folder' + os.sep + dateFolder)
         else:
             os.makedirs('D:/folder/' + dateFolder)
             for folder in folders:
                 files = os.listdir(work_path + os.sep + download_path + os.sep + folder)
                 for file in files:
                     shutil.copy(work_path + os.sep + download_path + os.sep + folder + os.sep + file,
-                                'D:/folder/' + dateFolder)
+                                'D:/folder' + os.sep + dateFolder)
         print('Копирование завершено')
         loadDataWindow.info_label.setText('Завершено')
 
@@ -117,6 +260,7 @@ class downloadProducts(QThread):
 
     def _download(self, products, path, zone):
         loadDataWindow.download_btn.setEnabled(False)
+        loadDataWindow.copy_btn.setEnabled(False)
         if len(products) > 0:
             if os.path.isdir(path):
                 try:
@@ -147,6 +291,7 @@ class downloadProducts(QThread):
                     print('Не удалось скачать данные для "{}"'.format(zone))
                     loadDataWindow.info_label.setText('Не удалось скачать данные для "{}"'.format(zone))
         loadDataWindow.download_btn.setEnabled(True)
+        loadDataWindow.copy_btn.setEnabled(True)
 
 
     def run(self):
@@ -162,6 +307,7 @@ class LoadDataWindow(QDialog):
         self.openMap_btn.clicked.connect(self.openMap)
         self.download_btn.clicked.connect(self.download)
         self.copy_btn.clicked.connect(self.copy_files)
+        self.geoTiff_CB.stateChanged.connect(self.imageCB)
 
         self.info_label.setVisible(False) #скрытие информационной строки
         self.dwnPath_label.setText(work_path + os.sep + download_path) #вывод полного пути до каталога загрузки
@@ -180,9 +326,20 @@ class LoadDataWindow(QDialog):
         for zone in zones:
             chkBox = self.LBItem(False)
             self.zonesList.addItem(chkBox)
-            chkBox.setText(zone)
+            chkBox.setText(zone.replace('_', ' '))
 
         self.zonesList.itemClicked.connect(self.zoneClicked) #событие нажатия элемента списка зон
+
+
+
+    def imageCB(self):
+        sender = self.sender()
+        if sender.checkState() == 2:
+            self.images_CB.setEnabled(True)
+        else:
+            self.images_CB.setEnabled(False)
+            self.images_CB.setCheckState(0)
+
 
     # Создание чекбокса в листбоксе
     def LBItem(self, checked):
@@ -209,8 +366,9 @@ class LoadDataWindow(QDialog):
         #print(sender.currentItem().checkState())
         if sender.currentItem().checkState() == 0: #если элемент был не отмечен
             sender.currentItem().setCheckState(Qt.Checked) #отметить
-            nTiles = tiles[zonesKeys[sender.row(sender.currentItem())]]
-            info = '{} - {}'.format(name, nTiles) #строка информации
+            #Tiles = tiles[zonesKeys[sender.row(sender.currentItem())]]
+            listTiles = tiles[sender.row(sender.currentItem())]
+            info = '{} - {}'.format(name, listTiles) #строка информации
             checked_items.append(zonesKeys[sender.row(sender.currentItem())])
             print(checked_items)
             self.zonesInfoList.appendPlainText(info)
@@ -222,18 +380,16 @@ class LoadDataWindow(QDialog):
             self.zonesInfoList.clear()
             for item in checked_items:
                 print('---{}---'.format(item))
-                name = zones[zonesKeys.index(item)]
+                name = zones[zonesKeys.index(item)].replace('_', ' ')
                 print(name)
                 #print(zonesKeys[item])
-                print(tiles[item])
-                nTiles = tiles[item]
+                #print(tiles[checked_items.index(item)])
+                nTiles = tiles[zonesKeys.index(item)]
                 print(nTiles)
                 info = '{} - {}'.format(name, nTiles)
                 self.zonesInfoList.appendPlainText(info)
             # for item in self.zonesInfoList.findItems(name, Qt.MatchContains): #по тексту выбранного элепента находится совпадение в списке информации
             #     self.zonesInfoList.takeItem(self.zonesInfoList.row(item)) # элемент удаляется
-
-
 
             #print('удалить')
         #print(sender.currentItem().checkState())
@@ -242,116 +398,36 @@ class LoadDataWindow(QDialog):
     def returnToMW(self):
         widget.setCurrentIndex(0)
 
-
-    # def dateInfo(self):
-    #     QDateFrom = self.dateFrom.date()  # дата начала в QDate формате
-    #     dateFrom = re.sub("-", "", str(QDateFrom.toPyDate()))  # перевод QDate вид в yyyymmdd формат
-    #     print(dateFrom)
-    #
-    #     QDateTo = self.dateTo.date()
-    #     dateTo = re.sub("-", "", str(QDateTo.toPyDate()))
-    #     print(dateTo)
-    #
-    #     date = [dateFrom, dateTo]
-    #     return date
-    #
-    # def cloudInfo(self):
-    #     try:
-    #         cloudFrom = int(self.cloudFrom.text())  # значения облачности
-    #         cloudTo = int(self.cloudTo.text())
-    #     except:
-    #         cloudFrom = 0  # при пустых значениях устанавливаются значения по умолчанию
-    #         cloudTo = 94
-    #         # print(type(cloudFrom))
-    #     print(cloudFrom)
-    #     print(cloudTo)
-    #
-    #     cloud = [cloudFrom, cloudTo]
-    #     return cloud
-
-    # def zonesDownload(self, dict_query_kwargs):
-    #     for zone in checked_items:
-    #         print(zone)
-    #         for tile in tiles[zone]:
-    #             products = OrderedDict()
-    #             try:
-    #                 print(tile)
-    #                 kwg = dict_query_kwargs.copy()
-    #                 kwg['tileid'] = tile
-    #                 requests = api.query(**kwg)
-    #                 products.update(requests)
-    #                 print('--------')
-    #                 #self.info_label.setText()
-    #             except:
-    #                 self.info_label.setVisible(True)
-    #                 self.info_label.setText(
-    #                     'Ошибка в введёной дате, либо Ошибка подключения к сервису ESA, проверьте адресс, логин и пароль')
-    #             list_keys = list(products.keys())
-    #         print('Найдено сцен в количестве {}'.format(len(products)))
-    #         for elem in list_keys:
-    #             print(products[elem]['title'])
-    #             self.zonesInfoList.appendPlainText(products[elem]['title'])
-    #             prod_inf = [products, zone]
-    #         return prod_inf
-    #         #self.zoneDownload(products, zone)
-
-    # def zoneDownload(self, products, zone):
-    #     if len(products) > 0:
-    #         if os.path.isdir(work_path + os.sep + download_path + os.sep + zone):
-    #             try:
-    #                 api.download_all(products, directory_path=work_path + os.sep + download_path + os.sep + zone,
-    #                                  max_attempts=5, checksum=True)
-    #                 self.info_label.setVisible(True)
-    #                 self.info_label.setText(
-    #                     'Данные скачаны в {}'.format(work_path + os.sep + download_path + os.sep + zone))
-    #                 print('Данные скачаны в {}',
-    #                       work_path + os.sep + download_path + os.sep + zone)
-    #             except:
-    #                 self.info_label.setVisible(True)
-    #                 self.info_label.setText('Не удалось скачать данные для "{}"'.format(zone))
-    #                 print('Не удалось скачать данные для "{}"'.format(zone))
-    #         else:
-    #             os.makedirs(work_path + os.sep + download_path + os.sep + zone)
-    #             try:
-    #                 api.download_all(products, directory_path=work_path + os.sep + download_path + os.sep + zone,
-    #                                  max_attempts=5, checksum=True)
-    #                 self.info_label.setVisible(True)
-    #                 self.info_label.setText(
-    #                     'Данные скачаны в {}'.format(work_path + os.sep + download_path + os.sep + zone))
-    #                 print('Данные скачаны в {}',
-    #                       work_path + os.sep + download_path + os.sep + zone)
-    #             except:
-    #                 self.info_label.setVisible(True)
-    #                 self.info_label.setText('Не удалось скачать данные для "{}"'.format(zone))
-    #                 print('Не удалось скачать данные для "{}"'.format(zone))
-
-
-
     def copy_files(self):
         self.info_label.setVisible(True)
-        print('Копирование в процессе...')
-        self.info_label.setText('Копирование в процессе...')
-        folders = os.listdir(work_path + os.sep + download_path)
-        print(folders)
-        a = str(datetime.date.today())
-        dateFolder = a.replace('-', '')
-        print(dateFolder)
-
-        if os.path.isdir('D:/folder/' + dateFolder):
-            for folder in folders:
-                files = os.listdir(work_path + os.sep + download_path + os.sep + folder)
-                for file in files:
-                    shutil.copy(work_path + os.sep + download_path + os.sep + folder + os.sep + file,
-                                'D:/folder/' + dateFolder)
-        else:
-            os.makedirs('D:/folder/' + dateFolder)
-            for folder in folders:
-                files = os.listdir(work_path + os.sep + download_path + os.sep + folder)
-                for file in files:
-                    shutil.copy(work_path + os.sep + download_path + os.sep + folder + os.sep + file,
-                                'D:/folder/' + dateFolder)
-        print('Копирование завершено')
-        self.info_label.setText('Копирование завершено')
+        try:
+            zones = os.listdir(work_path + os.sep + download_path)
+            print(zones)
+            a = datetime.date.today()
+            dateFolder = str(a.year)
+            print(dateFolder)
+            path = 'D:/PycharmProjects/automapizator/folder/' + dateFolder
+            if os.path.isdir(path):
+                for zone in zones:
+                    files = os.listdir(work_path + os.sep + download_path + os.sep + zone)
+                    for file in files:
+                        print('Копирование в процессе...')
+                        self.info_label.setText('Копирование в процессе...')
+                        shutil.copy(work_path + os.sep + download_path + os.sep + zone + os.sep + file,
+                                    path)
+            else:
+                os.makedirs(path)
+                for zone in zones:
+                    files = os.listdir(work_path + os.sep + download_path + os.sep + zone)
+                    for file in files:
+                        print('Копирование в процессе...')
+                        self.info_label.setText('Копирование в процессе...')
+                        shutil.copy(work_path + os.sep + download_path + os.sep + zone + os.sep + file,
+                                    path)
+            print('Копирование завершено')
+            self.info_label.setText('Копирование завершено')
+        except FileNotFoundError:
+            self.info_label.setText('Сначала загрузите данные, для создания нужных каталогов')
 
 
     #функция загрузки
@@ -359,7 +435,6 @@ class LoadDataWindow(QDialog):
 
         self.info_label.setVisible(True)
         # self.info_label.setText('Идёт загрузка. Ожидайте')
-
 
         #date = self.dateInfo()
         # self.download_btn.setEnabled(False)  # отключение кнопки загрузки
@@ -406,8 +481,8 @@ class LoadDataWindow(QDialog):
 
         #перебор выбранных зон для скачивания
         for zone in checked_items:
-            print(zone)
-            for tile in tiles[zone]:
+            print('Зона - {}'.format(zone))
+            for tile in tiles[checked_items.index(zone)]:
                 products = OrderedDict()
                 try:
                     print(tile)
